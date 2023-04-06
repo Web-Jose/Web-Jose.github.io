b855c1f4ed4492871c6db64969a731ae1294869a
Web-Jose <119899203+Web-Jose@users.noreply.github.com>
2023-04-06 11:26:24 -0700

    Updated js and html

--- portfolio.js
+++ portfolio.js
@@ -49,3 +49,37 @@ fetch("projects.json")
       cardContainer.appendChild(cardElement);
     });
   });
+
+// Get all the navigation links
+const navLinks = document.querySelectorAll("nav a");
+
+// Add a scroll event listener to the window
+window.addEventListener("scroll", () => {
+  // Get the top of the viewport
+  const topViewport = window.pageYOffset;
+
+  // Loop through each section on the page
+  document.querySelectorAll("section").forEach((section) => {
+    // Get the top and bottom of the section
+    const topSection = section.offsetTop;
+    const bottomSection = topSection + section.offsetHeight;
+
+    // Check if the top of the viewport is within the section
+    if (topViewport >= topSection && topViewport < bottomSection) {
+      // Get the ID of the section
+      const sectionId = section.getAttribute("id");
+
+      // Loop through each navigation link
+      navLinks.forEach((navLink) => {
+        // Check if the navigation link corresponds to the section
+        if (navLink.getAttribute("href") === `#${sectionId}`) {
+          // Add an "active" class to the navigation link
+          navLink.classList.add("active");
+        } else {
+          // Remove the "active" class from the other navigation links
+          navLink.classList.remove("active");
+        }
+      });
+    }
+  });
+});
