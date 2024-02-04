23266c0982bb2a1dd8c7f8728b88bafb2622ef14
Web-Jose <119899203+Web-Jose@users.noreply.github.com>
2024-02-03 16:19:14 -0800

    PHP Header

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<!-- Header -->
+<header>
+    <a href="/" class="Title">Code By Jose</a>
+    <!-- Navigation Bar -->
+    <nav>
+      <a href="index.html#Home"
+        ><i class="fa-solid fa-house" style="color: #d2d9d8"></i>Home</a
+      >
+      <a href="index.html#aboutmeSec"
+        ><i class="fa-solid fa-user" style="color: #d2d9d8"></i>About</a
+      >
+      <a href="index.html#projects"
+        ><i class="fa-solid fa-folder" style="color: #d2d9d8"></i>Projects</a
+      >
+      <!--<a href="#experiences">Experiences</a>--->
+      <a href="index.html#contact"
+        ><i class="fa-solid fa-message" style="color: #d2d9d8"></i>Contact</a
+      >
+      <a href="JoseC-CV.pdf" download="JoseC-CV.pdf"
+        ><i class="fa-solid fa-cloud-arrow-down" style="color: #d2d9d8"></i
+        >Resume</a
+      >
+    </nav>
+  </header>
